1d95fd62d0a4c3a2763e64c74f56860066fb8950
Jeff <mrjeffrey1011@gmail.com>
2022-08-30 10:23:28 -0400

    rating component updated

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -4,39 +4,39 @@ import { FaStarHalf, FaStar} from 'react-icons/fa'
 const Rating = ({value, text, color}) => {
 
     if(value === 5){
-        return(<div><FaStar/><FaStar/><FaStar/><FaStar/><FaStar/> <span>{text}</span>
+        return(<div><FaStar/><FaStar/><FaStar/><FaStar/><FaStar/> <span>({text})</span>
         </div>) 
     }
     if(value >= 4.5){
-        return(<div><FaStar/><FaStar/><FaStar/><FaStar/><FaStarHalf/> <span>{text}</span>
+        return(<div><FaStar/><FaStar/><FaStar/><FaStar/><FaStarHalf/> <span>({text})</span>
         </div>) 
     }
     if(value === 4){
-        return(<div><FaStar/><FaStar/><FaStar/><FaStar/> <span>{text}</span>
+        return(<div><FaStar/><FaStar/><FaStar/><FaStar/> <span>({text})</span>
         </div>) 
     }
     if(value >= 3.5){
-        return(<div><FaStar/><FaStar/><FaStar/><FaStarHalf/> <span>{text}</span>
+        return(<div><FaStar/><FaStar/><FaStar/><FaStarHalf/> <span>({text})</span>
         </div>) 
     }
     if(value === 3){
-        return(<div><FaStar/><FaStar/><FaStar/> <span>{text}</span>
+        return(<div><FaStar/><FaStar/><FaStar/> <span>({text})</span>
         </div>) 
     }
     if(value >= 2.5){
-        return(<div><FaStar/><FaStar/><FaStarHalf/> <span>{text}</span>
+        return(<div><FaStar/><FaStar/><FaStarHalf/> <span>({text})</span>
         </div>) 
     }
     if(value === 2){
-        return(<div><FaStar/><FaStar/> <span>{text}</span>
+        return(<div><FaStar/><FaStar/> <span>({text})</span>
         </div>) 
     }
     if(value >= 1.5){
-        return(<div><FaStar/><FaStarHalf/> <span>{text}</span>
+        return(<div><FaStar/><FaStarHalf/> <span>({text})</span>
         </div>) 
     }
     if(value === 1){
-        return(<div><FaStar/> <span>{text}</span>
+        return(<div><FaStar/> <span>({text})</span>
         </div>) 
     }
     return (
