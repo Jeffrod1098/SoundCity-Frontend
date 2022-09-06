b9a5ece5a5090b45f5ca6170ae6d0d867fa9ff2b
Jeff <mrjeffrey1011@gmail.com>
2022-09-05 23:27:42 -0400

    card added for cart items

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -12,6 +12,9 @@ const CartPage = () => {
     const { id } = useParams()
     const qty = 1
 
+    const cart = useSelector(state => state.cart)
+    const {cartItems}= cart
+    console.log(cartItems)
     useEffect(() => {
         if(id){
             dispatch(addToCart(id, qty))
@@ -21,7 +24,32 @@ const CartPage = () => {
 
     return(
         <div>
-            Cart Page 
+            <div class="card">
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-128x128">
+          <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image"/>
+        </figure>
+      </div>
+      <div class="media-content">
+        <p class="title is-4">name</p>
+      </div>
+      <div>
+      <p class="subtitle is-6">@johnsmith</p>
+      </div>
+    </div>
+
+    <div class="content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+      <a href="#">#css</a> <a href="#">#responsive</a>
+      <br/>
+      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+    </div>
+  </div>
+</div>
+
         </div>
     )
 }
