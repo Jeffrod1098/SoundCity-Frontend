c647f17b10a62ebfc4999f91b77197408f4f03d8
Jeff <mrjeffrey1011@gmail.com>
2022-09-05 23:38:42 -0400

    card in carts page ready for data

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -13,43 +13,43 @@ const CartPage = () => {
     const qty = 1
 
     const cart = useSelector(state => state.cart)
-    const {cartItems}= cart
+    const { cartItems } = cart
     console.log(cartItems)
     useEffect(() => {
-        if(id){
+        if (id) {
             dispatch(addToCart(id, qty))
         }
 
-    },[dispatch, id, qty])
+    }, [dispatch, id, qty])
 
-    return(
-        <div>
-            <div class="card">
-  <div class="card-content">
-    <div class="media">
-      <div class="media-left">
-        <figure class="image is-128x128">
-          <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image"/>
-        </figure>
-      </div>
-      <div class="media-content">
-        <p class="title is-4">name</p>
-      </div>
-      <div>
-      <p class="subtitle is-6">@johnsmith</p>
-      </div>
-    </div>
+    return (
+        <div className="columns">
+            <div className="column is-2"></div>
+            <div className="card column is-8">
+                <div className="card-content">
+                    <div className="media">
+                        <div className="media-left">
+                            <figure className="image is-128x128">
+                                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+                            </figure>
+                        </div>
+                        <div className="media-content">
+                            <p className="title is-4">name</p>
+                        </div>
+                        <div>
+                            <p className="subtitle is-6">@johnsmith</p>
+                        </div>
+                    </div>
 
-    <div class="content">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-      <a href="#">#css</a> <a href="#">#responsive</a>
-      <br/>
-      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-    </div>
-  </div>
-</div>
-
+                    <div className="content">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                        <a href="#">#css</a> <a href="#">#responsive</a>
+                        <br />
+                    </div>
+                </div>
+            </div>
+            <div className="column is-2"></div>
         </div>
     )
 }
