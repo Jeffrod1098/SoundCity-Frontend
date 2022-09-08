a60900830a62342bbfe8b0b959e671599ccaca4d
Jeff <mrjeffrey1011@gmail.com>
2022-09-07 23:07:58 -0400

    cartpage updates

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -11,7 +11,6 @@ const CartPage = () => {
     const dispatch = useDispatch()
     const { id } = useParams()
     const qty = 1
-
     const cart = useSelector(state => state.cart)
     const { cartItems } = cart
     useEffect(() => {
@@ -20,6 +19,7 @@ const CartPage = () => {
         }
 
     }, [dispatch, id, qty])
+    console.log(cartItems)
 
     return (
         <div className="columns">
@@ -30,21 +30,24 @@ const CartPage = () => {
                 <div>
                     <h2>Your cart is currently empty</h2>
                 </div>
-            ): (
+            ): (cartItems.map(item => (
                 <div className="card column is-7">
                 <div className="card-content">
                     <div className="media">
                         <div className="media-left">
                             <figure className="image is-128x128">
-                                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+                                <img src={item.image} />
                             </figure>
                         </div>
+                        <Link to={`/product/${item.product}`}>
                         <div className="media-content">
-                            <p className="title is-4">name</p>
+                            <p className="title is-4">{item.name}</p>
                         </div>
+                        </Link>
                         <div>
-                            <p className="subtitle is-6">@johnsmith</p>
+                            <p className="subtitle is-6">${item.price}</p>
                         </div>
+ 
                     </div>
 
                     <div className="content">
@@ -55,6 +58,10 @@ const CartPage = () => {
                     </div>
                 </div>
             </div>
+            ))
+
+            
+
             )}
 
             <div className="column is-4"></div>
