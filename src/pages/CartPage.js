bdf6e23e3d89892f7986bf6ddc8167f9b0c51985
Jeff <mrjeffrey1011@gmail.com>
2022-09-07 23:19:27 -0400

    remove button added no function

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
 import { addToCart } from "../actions/cartActions"
 import { useParams } from "react-router-dom"
+import { FaTrashAlt } from "react-icons/fa"
 
 
 
@@ -30,37 +31,46 @@ const CartPage = () => {
                 <div>
                     <h2>Your cart is currently empty</h2>
                 </div>
-            ): (cartItems.map(item => (
+            ) : (cartItems.map(item => (
                 <div className="card column is-7">
-                <div className="card-content">
-                    <div className="media">
-                        <div className="media-left">
-                            <figure className="image is-128x128">
-                                <img src={item.image} />
-                            </figure>
-                        </div>
-                        <Link to={`/product/${item.product}`}>
-                        <div className="media-content">
-                            <p className="title is-4">{item.name}</p>
-                        </div>
-                        </Link>
-                        <div>
-                            <p className="subtitle is-6">${item.price}</p>
-                        </div>
- 
-                    </div>
+                    <div className="card-content">
+                        <div className="media">
+                            <div className="media-left">
+                                <figure className="image is-128x128">
+                                    <img src={item.image} />
+                                </figure>
+                            </div>
+                            <div className="media-content">
+                                <Link to={`/product/${item.product}`}>
+                                    <p className="title is-4">{item.name}</p>
+                                </Link>
+                            </div>
 
-                    <div className="content">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                        <a href="#">#css</a> <a href="#">#responsive</a>
-                        <br />
+                            <div>
+                                <p className="subtitle is-6">${item.price}</p>
+                            </div>
+
+                        </div>
+
+                        <div className="content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                            <a href="#">#css</a> <a href="#">#responsive</a>
+                            <button className="button is-light">
+                                <div className="icon is-small">
+                                    <FaTrashAlt />
+                                </div>
+                                <div>
+                                    remove
+                                </div>
+                            </button>
+                            <br />
+                        </div>
                     </div>
                 </div>
-            </div>
             ))
 
-            
+
 
             )}
 
