8887bb2d502a38ea4c446241d755511802c3d074
Jeff <mrjeffrey1011@gmail.com>
2022-10-07 12:39:27 -0400

    added register page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,12 +1,18 @@
 import React from "react"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { login } from "../actions/userActions"
 
 const LoginPage = () => {
 
-    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    
+    const submitHandler = (e) => {
+        e.preventDefault()
+        console.log('submitted')
+    }
 
 
     return (
@@ -14,18 +20,18 @@ const LoginPage = () => {
         <div className='form'>
             {/* <h1 className="title is-2"></h1> */}
             <div className="container has-text-centered box" style={{ maxWidth: '300px' }}>
-                <form>
+                <form onSubmit={submitHandler}>
                     <div className="field">
                         <label className="label" >Username</label>
                         <div className="control">
-                            <input className="input" name="username" type="text" placeholder="username" required onChange={e => setUsername(e.target.value)} />
+                            <input className="input" name="username" type="email" placeholder="Enter Email" required onChange={e => setEmail(e.target.value)} />
                         </div>
                     </div>
 
                     <div className="field">
                         <label className="label" >Password</label>
                         <div className="control">
-                            <input className="input" name="password" type="password" placeholder="password" required onChange={e => setPassword(e.target.value)} />
+                            <input className="input" name="password" type="password" placeholder="Enter Password" required onChange={e => setPassword(e.target.value)} />
                         </div>
                     </div>
 
@@ -35,7 +41,12 @@ const LoginPage = () => {
                         </div>
                     </div>
                 </form>
+
             </div>
+            <Link to={'/register'}>
+            <div className="has-text-centered">Click here to register</div>            
+            </Link>
+
         </div>
         </div>
     )
