4a998ed7dcc1e669545e5c6f82b445bf32d018f3
Jeff <mrjeffrey1011@gmail.com>
2022-10-12 20:40:14 -0400

    setting up register page

---
+++ src/pages/RegisterPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { register } from "../actions/userActions"
+import { useLocation } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+
+
+const RegisterPage = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
