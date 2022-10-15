d5c934e5230167cebec9524a4a6da7bac5107332
Jeff <mrjeffrey1011@gmail.com>
2022-10-15 10:38:49 -0400

    routing profile page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {Route, Routes, Link} from 'react-router-dom'
 import CartPage from './pages/CartPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path='/cart/:id' element ={<CartPage/>} />
           <Route path='/login' element={<LoginPage/>} />
           <Route path='/register' element={<RegisterPage/>}/>
+          <Route path='/profile' elment={<ProfilePage/>}/>
         </Routes>
       </main>
       {/* <Footer /> */}
--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { useLocation } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import ErrorMessage from "../components/ErrorMessage";
+import Loader from "../components/Loader";
+import { getUserDetails } from "../actions/userActions";
 
 const ProfilePage = () => {
     return (
         <div>
-
+            <h3>User Profile</h3>
         </div>
     )
 }
